1455b779b35cf556c3fb780afb756ce91f756b29
Ana Patiño <asofiapatino@unicesar.edu.co>
2022-06-28 11:07:14 -0500

    login

--- src/app/components/principal/login/login.component.ts
+++ src/app/components/principal/login/login.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+
+interface Roles {
+  name: string;
+}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  hide = true;
+
+  generalInformationFormGroup = this._formBuilder.group({
+    generalCtrl: ['', Validators.required],
+  });
+
+  rolesControl = new FormControl<Roles | null>(null, Validators.required);
+  selectFormControl = new FormControl('', Validators.required);
+  roles: Roles[] = [
+    { name: 'Estudiante' },
+    { name: 'Docente' },
+    { name: 'Comite Evaluativo' },
+  ];
+
+  constructor(private router: Router, private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
 
