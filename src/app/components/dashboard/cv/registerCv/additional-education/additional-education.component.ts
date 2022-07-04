ec7d2043d54383ebb92a92dbf04324a15bf3db3a
Ana Patiño <asofiapatino@unicesar.edu.co>
2022-07-04 18:56:47 -0500

    arrangements in the resume

--- src/app/components/dashboard/cv/registerCv/additional-education/additional-education.component.ts
+++ src/app/components/dashboard/cv/registerCv/additional-education/additional-education.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+
+interface Department {
+  name: string;
+}
+
+interface City {
+  name: string;
+}
 
 @Component({
   selector: 'app-additional-education',
   templateUrl: './additional-education.component.html',
   styleUrls: ['./additional-education.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
 })
 export class AdditionalEducationComponent implements OnInit {
+  cityControl = new FormControl<City | null>(null, Validators.required);
+  selectFormControl = new FormControl('', Validators.required);
+  cities: City[] = [{ name: '-' }, { name: '-' }];
+
+  departmentControl = new FormControl<Department | null>(
+    null,
+    Validators.required
+  );
+  selectDepartmentFormControl = new FormControl('', Validators.required);
+  departments: Department[] = [{ name: '-' }, { name: '-' }];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/components/dashboard/cv/registerCv/education/education.component.ts
+++ src/app/components/dashboard/cv/registerCv/education/education.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
+interface Department {
+  name: string;
+}
+
+interface City {
+  name: string;
+}
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
@@ -17,6 +25,17 @@ export class EducationComponent implements OnInit {
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
   });
+
+  cityControl = new FormControl<City | null>(null, Validators.required);
+  selectFormControl = new FormControl('', Validators.required);
+  cities: City[] = [{ name: '-' }, { name: '-' }];
+
+  departmentControl = new FormControl<Department | null>(
+    null,
+    Validators.required
+  );
+  selectDepartmentFormControl = new FormControl('', Validators.required);
+  departments: Department[] = [{ name: '-' }, { name: '-' }];
 
   constructor(private _formBuilder: FormBuilder) {}
 
